ae50fb3dcebd497560a6aa1388461c3795f21539
JayAwesome <jayawesomest@gmail.com>
2019-08-02 21:46:46 +0100

    added save menu

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -3,6 +3,8 @@ package com.example.travelmantics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class InsertActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class InsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return  true;
     }
 }
