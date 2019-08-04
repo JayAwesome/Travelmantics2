14c0e2d57d90188bcabb76c7186174b67a208965
JayAwesome <jayawesomest@gmail.com>
2019-08-04 11:24:10 +0100

    Auth Administrator

--- app/src/main/java/com/example/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/example/travelmantics/FirebaseUtil.java
@@ -1,12 +1,17 @@
 package com.example.travelmantics;
 
 import android.app.Activity;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,6 +29,7 @@ public class FirebaseUtil {
     private static final int RC_SIGN_IN = 123;
     private static Activity caller;
     private FirebaseUtil(){};
+    public static boolean isAdmin;
 
     public static void openFbReference(String ref, final Activity callerActivity){
         if (firebaseUtil == null) {
@@ -36,6 +42,10 @@ public class FirebaseUtil {
                 public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                     if (firebaseAuth.getCurrentUser() == null) {
                         FirebaseUtil.signIn();
+                    }
+                    else {
+                        String userId = firebaseAuth.getUid();
+                        checkAdmin(userId);
                     }
 
                     Toast.makeText(callerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG).show();
@@ -60,6 +70,40 @@ public class FirebaseUtil {
                         .build(),
                 RC_SIGN_IN);
 
+    }
+
+    private static void checkAdmin(String uid) {
+        FirebaseUtil.isAdmin=false;
+        DatabaseReference ref =mFirebaseDatabase.getReference().child("administrators")
+                .child(uid);
+        ChildEventListener listener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                FirebaseUtil.isAdmin=true;
+                Log.d("Admin", "You are an administrator");
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        ref.addChildEventListener(listener);
     }
 
     public static void attachListener(){
